6673ea338698d2f22a900780ee721f23e7af218d
George <gosha3484@gmail.com>
2026-02-24 20:01:20 +0200

    added CustomerController and Repository

---
+++ src/main/java/com/georgiia/demo/conrtoller/CustomerController.java
@@ -0,0 +1,57 @@
+package com.georgiia.demo.conrtoller;
+
+import com.georgiia.demo.entity.Customer;
+import com.georgiia.demo.repository.CustomerRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerRepository repo;
+
+    public CustomerController(CustomerRepository repo) {
+        this.repo = repo;
+    }
+
+
+    @GetMapping
+    public List<Customer> getAll() {
+        return repo.findAll();
+    }
+
+    // GET
+    @GetMapping("/{id}")
+    public Customer getById(@PathVariable Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id " + id));
+    }
+
+    // POST
+    @PostMapping
+    public Customer create(@RequestBody Customer customer) {
+        return repo.save(customer);
+    }
+
+    // PUT
+    @PutMapping("/{id}")
+    public Customer update(@PathVariable Long id, @RequestBody Customer customer) {
+        Customer existing = repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("customer not found with id " + id));
+
+        existing.setFirstName(customer.getFirstName());
+        existing.setLastName(customer.getLastName());
+        existing.setEmail(customer.getEmail());
+        existing.setCustomersPhone(customer.getCustomersPhone());
+        return repo.save(existing);
+    }
+
+    // DELETE /products/{id}
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repo.deleteById(id);
+    }
+}
